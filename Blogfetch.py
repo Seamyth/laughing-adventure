e645d9a69c2aecc44ff7796f98df1712a5774847
Seamyth <ameysathe21@gmail.com>
2016-12-30 11:12:01 +0530

    Update Blogfetch.py

--- Blogfetch.py
+++ Blogfetch.py
@@ -1,3 +1,6 @@
+##from page we will extract the page in link and then we will feed it to the soup using BeautifulSoup module
+##then with loops we will print only the labels from the blog
+
 from bs4 import BeautifulSoup
 import urllib.request as ur
 
